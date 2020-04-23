4401acf3caf70e14d1937ab6e09b886b7eb02e73
james-a-mcmanus <47943540+james-a-mcmanus@users.noreply.github.com>
2020-04-23 11:34:41 +0100

    separated out main_function

--- src/MushroomBody.jl
+++ src/MushroomBody.jl
@@ -20,21 +20,93 @@ function run_model()
 	nn = SA[10, 100, 1]
 	numsteps  = 100
 	in1 = create_input(nn[1], 350:450, numsteps, numsteps, [50,50], 0.8, BAstart=10)
-	run_model(in1)
+	run_model(in1, nn)
 end
 
+function test_model(in1, weights)
 
-function train_model()
-	a=3
-	#some shite here
+ 5
 end
 
+function train_model(in1)
 
-function run_model(in1)
+	activation, 
+	rec, 
+	spiked, 
+	spt, 
+	I, 
+	ACh, 
+	input, 
+	synapses,
+	weights,
+	γ, 
+	da = initialise_matrices()
+	
+end
+
+function innerlayerupdates!(l, ACh, synt, Φ, t, spt, input, weights, rev, activation, γ, synapses, da, tconst)
+
+	update_ACh!(ACh[l], synt[l], Φ[l], t, spt[l])
+	calc_input!(input[l+1], weights[l], ACh[l], rev[l], activation[l+1])
+	update_weights!(weights[l], γ[l], synapses[l], t, spt[l], spt[l+1], da, tconst[l]; δt=1, A₋=-1, t₋=15)
+end
+
+function inputandreward(t, input, in1, da, τ)
+
+	input[1] .= in1.inarrayseq[t]==1 ? in1.inarray[1] : input[1]
+	BA = in1.BAseq[t]
+	da = update_da(da, BA, τ)
+
+	return(BA,da)	
+end
+
+function run_model(in1::RandInput, nn; showplot=false)
 
 	# preallocating all the parameters
-	nn = SA[10, 100, 1]
 	numsteps  = 100
+
+	#something here like initialiseplotfunction that takes 
+	#in the function defined at the top
+
+	plt = Dashplot()
+
+	activation, 
+	rec, 
+	spiked, 
+	spt, 
+	I, 
+	ACh, 
+	input, 
+	synapses,
+	weights,
+	γ, 
+	da = initialise_matrices(nn)
+
+	for t = 1:numsteps
+
+		BA, da = inputandreward(t, input,in1, da, τ)
+		
+		for l = 1:length(nn)
+
+			update_activation!(nn[l], activation[l], vr[l], spiked[l], spt[l], t, vt[l], rec[l], input[l], C[l], a[l], b[l], c[l], d[l], k[l])
+
+			if l != length(nn)
+
+				innerlayerupdates!(l, ACh, synt, Φ, t, spt, input, weights, rev, activation, γ, synapses, da, tconst)
+				
+			end
+
+		end
+
+		sl=1
+		dashboard(plt, t, weights[sl], activation[sl], spt[sl], da, rec[sl])
+
+	end
+
+	return(synapses, weights)
+end
+
+function initialise_matrices(nn)
 
 	# neuron types
 	activation = [Vector{Float64}(undef,i) for i in nn]
@@ -47,7 +119,7 @@ function run_model(in1)
 	#synapse types
 	synapses = [spzeros(nn[i], nn[i+1]) for i = 1:length(nn)-1]
 	γ = [spzeros(nn[i], nn[i+1]) for i = 1:length(nn)-1]
-	da = [0.0,0.0,0.0]
+	da = 0.0
 
 	# create layers
 	fillcells!(activation,-60.0)
@@ -58,38 +130,10 @@ function run_model(in1)
 	fillcells!(ACh, 0.0)
 	fillcells!(input, 0.0)
 
-	#something here like initialiseplotfunction that takes 
-	#in the function defined at the top 
-	plt = Dashplot()
-
 	create_synapses!(synapses, 1)
-	weights = 2 .* synapses
+	weights = 2 .* synapses	
 
-	for t = 1:numsteps
-
-		input[1] .= in1.inarrayseq[t]==1 ? in1.inarray[1] : input[1]
-		BA = in1.BAseq[t]
-
-		for l = 1:length(nn)
-
-			update_activation!(nn[l], activation[l], vr[l], spiked[l], spt[l], t, vt[l], rec[l], input[l], C[l], a[l], b[l], c[l], d[l], k[l])
-			
-			da[l] = update_da(da[l], BA, τ[l])
-
-			if l != length(nn)
-
-				# output functions etc.
-				update_ACh!(ACh[l], synt[l], Φ[l], t, spt[l])
-				calc_input!(input[l+1], weights[l], ACh[l], rev[l], activation[l+1])
-				update_weights!(weights[l], γ[l], synapses[l], t, spt[l], spt[l+1], da[l], tconst[l]; δt=1, A₋=-1, t₋=15)
-
-			end
-
-		end
-
-		dashboard(plt, t, weights[sl], activation[sl], spt[sl], da[sl], rec[sl])
-
-	end
-
+	return activation, rec, spiked, spt, I, ACh, input, synapses, weights, γ, da
 end
+
 end # module
