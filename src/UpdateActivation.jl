64c9eb4ae87816ec039c9c58f012b76e785e9665
james-a-mcmanus <jmcmanus.spam@gmail.com>
2020-06-04 15:30:04 +0100

    range of normalisation functions added

--- src/UpdateActivation.jl
+++ src/UpdateActivation.jl
@@ -7,12 +7,9 @@ function update_activation!(t, layer, nn, matrices, parameters)
 	ξ = generate_noise(σ,nn[layer])	
 
 	update_voltage!(v, vr, vt, rec, I, ξ, C, k, δt)
-	
 	update_recovery!(v, vr, rec, a, b, δt)
 	update_spikes!(v, sp, spt, t, vt, rec, c, d)
-	#layer==2 && apl_inhibition2!(matrices, parameters)
 end	
-
 
 function generate_noise(σ,nn)
 
@@ -48,15 +45,39 @@ function calc_input!(l, m, p; rev=0)
 
 	input .= dropdims(sum( @.(w * ACh * (rev - v)'), dims=1), dims=1) #dropdims(sum(w .* ACh .* (rev .- v)', dims = 1), dims=1)
 
-	do_normalise(l) &&  divisive_normalise!(input, p.normalise_to[l])
-
+	l==1 && winner_takes_all(input, 100)
 end
-
-do_normalise(layer) = layer==1 
 
 function divisive_normalise!(input, inmax)
 	if sum(input) == 0
 		return
 	end
 	input .= inmax * input ./ sqrt(sum(input .^ 2))
+end
+
+function subtractive_normalise(input, λ)
+
+	input = - λ * sum(input) / length(input)
+end
+
+function winner_takes_all(input, numwinners)
+
+	anti_assign(input, maxk(input,numwinners), 0.0)
+end
+
+function maxk!(ix, a, k; initialized=false)
+    partialsortperm!(ix, a, 1:k, rev=true, initialized=initialized)
+    return ix
+end
+
+function maxk(a,k)
+
+	partialsortperm(a,1:k,rev=true)
+end
+
+function anti_assign(array, indices, assign_otherwise)
+
+	for i in 1:length(array)
+		array[i] = i in indices ? array[i] : assign_otherwise
+	end
 end
